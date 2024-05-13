aac7980cfefce45a708cc5e58019867396ac0961
Similimodo <50338859+Similimodo@users.noreply.github.com>
2024-05-13 16:47:29 -0400

    Delete 07_Linux_String_Substitute_sed.sh

--- 07_Linux_String_Substitute_sed.sh
+++
@@ -1,9 +0,0 @@
-#Linux String Substitute (sed)
-
-ssh steve@stapp02
-
-#removed the line that has the word "code"
- sed -e '/code/d' BSD.txt > /home/BSD_DELETE.txt
-
-#changed the word "and" to "is"
- sed -e 's/and/is/g' BSD.txt > /home/BSD_REPLACE.txt

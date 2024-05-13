cdf93f7288275b76807a0b1be32d3489b195e210
Similimodo <paschalonor@gmail.com>
2024-05-13 11:27:55 -0400

    commit

---
+++ linux/06_dns_troubleshooting.sh
@@ -0,0 +1,10 @@
+
+APP Server 2
+ssh steve@stapp02
+sudo su
+cat /etc/resolv.conf
+vim /etc/resolv.conf
+add the name of the server and the ip you want to resolve (Google Dns)
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+
